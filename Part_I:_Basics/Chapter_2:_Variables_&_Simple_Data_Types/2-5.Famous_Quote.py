10808e15eaa352b5743e971c91f9401514739df2
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-26 16:17:42 -0600

    Chapter 2

---
+++ Part_I:_Basics/Chapter_2:_Variables_&_Simple_Data_Types/2-5.Famous_Quote.py
@@ -0,0 +1,3 @@
+quote = '"A high degree of intellect tends to make a man unsocial."'
+person = "Arthur Schopenhauer".title()
+print(person, 'once said,', quote)
---
+++ Part_I:_Basics/Chapter_2:_Variables_&_Simple_Data_Types/2-6.Famous_Quote_2.py
@@ -0,0 +1,3 @@
+message = '“A high degree of intellect tends to make a man unsocial.”'
+famous_person = 'Arthur Schopenhauer'
+print(f"{famous_person} once said, {message}")
