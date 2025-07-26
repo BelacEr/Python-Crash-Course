49e92091f9113353255972099304cb449d7a9594
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-26 16:18:03 -0600

    Chapter 2

---
+++ Part_I:_Basics/Chapter_2:_Variables_&_Simple_Data_Types/2-7.Stripping_Names.py
@@ -0,0 +1,5 @@
+author1 = 'Arthur Schopenhauer'
+author2 = ' Lev Nikolayevich Tolstoy '
+author3 = '     Benito Pérez Galdós     '
+
+print(f"My favorite authors are: \n\t{author1} \n\t{author2.strip()} \n\t{author3.strip()}")
