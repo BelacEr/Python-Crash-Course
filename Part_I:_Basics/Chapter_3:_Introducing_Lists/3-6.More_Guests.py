46d002f98c945aec8c21272d7344f886a2a22813
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-27 14:43:11 -0600

    Chapter 3

---
+++ Part_I:_Basics/Chapter_3:_Introducing_Lists/3-6.More_Guests.py
@@ -0,0 +1,14 @@
+guests = ['Ana', 'Gabo', 'Miguel', 'Maressa', 'Arix', 'Monika']
+
+def dinner(guests_list):
+    for i in range(len(guests_list)):
+        print(f'I would like to have a dinner with you {guests_list[i]}.')    
+
+dinner(guests)  
+
+print('I found a bigger table.')
+# add new friends
+guests.insert(3, 'Ariel')
+guests.append('Hypervisor')
+
+dinner(guests)  # call dinner function with changed list
