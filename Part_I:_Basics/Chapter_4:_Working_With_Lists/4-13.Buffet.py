a5df59e2925d9d0b5bed15109e9a7b39edc057bf
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-30 08:31:12 -0600

    Chapter 4

---
+++ Part_I:_Basics/Chapter_4:_Working_With_Lists/4-13.Buffet.py
@@ -0,0 +1,24 @@
+foods = (  
+    "Grilled Chicken",
+    "Mashed Potatoes",
+    "Steamed Vegetables",
+    "Pasta with Marinara Sauce",
+    "Garden Salad"
+)
+
+print('Restaurant menu:')
+for food in foods:
+    print(food)
+
+buffet_foods = (        # replace two of the items with different foods
+    "Grilled Chicken",
+    "Mashed Potatoes",
+    "Steamed Vegetables",
+    "Roast Beef",
+    "Macaroni and Cheese"
+)
+
+print('\nNew restaurant menu:')
+for food in foods:
+    print(food)
+
