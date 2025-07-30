b1ea94e66e2a2286bcb442cf8b2aa5432b25e59d
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-30 15:10:36 -0600

    Chapter 5

---
+++ Part_I:_Basics/Chapter_5:_If_Statements/5-7.Favorite_Fruit.py
@@ -0,0 +1,18 @@
+# List of favorite fruits
+favorite_fruits = ['bananas', 'strawberries', 'apples']
+
+# Series of independent if statements checking for certain fruits
+if 'bananas' in favorite_fruits:
+    print("You really like bananas!")
+
+if 'apples' in favorite_fruits:
+    print("You really like apples!")
+
+if 'strawberries' in favorite_fruits:
+    print("You really like strawberries!")
+
+if 'blueberries' in favorite_fruits:
+    print("You really like blueberries!")
+
+if 'mangoes' in favorite_fruits:
+    print("You really like mangoes!")
