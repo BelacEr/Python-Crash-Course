3375fb770eff89e712e11f9b1305a115a30258a4
BelacEr <caleblopezmorgado2007@gmail.com>
2025-07-30 15:58:08 -0600

    Chapter 5

---
+++ Part_I:_Basics/Chapter_5:_If_Statements/5-9.No_Users.py
@@ -0,0 +1,12 @@
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if 'admin' == username:
+            print(f'Hello {username},  would you like to see a status report?')
+        else:
+            print(f'Hello {username}, thank you for logging in again.')
+else:
+    print('We need to find some users!')
+
+
