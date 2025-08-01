de352226ae92f343af5a0bb7a08e072dad3dc70f
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-01 14:28:59 -0600

    Chapter 6

---
+++ Part_I:_Basics/Chapter_6:_Dictionaries/6-2.Favorite_Numbers.py
@@ -0,0 +1,12 @@
+favorite_numbers = {
+    'Gabo': 13,
+    'Isabela': 21,
+    'Monika': 7,
+    'Ana': 3,
+    'Caleb': 7,
+        }
+
+# print each person's name and favorite's number
+for person, number in favorite_numbers.items():
+    print(f"{person}'s favorite number is: {number}")
+
