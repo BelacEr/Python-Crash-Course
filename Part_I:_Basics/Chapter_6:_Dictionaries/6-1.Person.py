0d2dabc80a4450224b357f18663f6dfdd0b7e6a7
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-01 14:17:50 -0600

    Chapter 6

---
+++ Part_I:_Basics/Chapter_6:_Dictionaries/6-1.Person.py
@@ -0,0 +1,8 @@
+information = {
+    'first_name': 'Isabela',
+    'last_name': 'Reyes',
+    'age': 18,
+    'city': 'Mexico City',
+        }
+
+print(f"{information['first_name']} {information['last_name']} is {information['age']} years old and lives in {information['city']}.")
