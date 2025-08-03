b0461faaf36dd9ca16ee7daf6b458fb65f68338d
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-03 16:23:30 -0600

    Chapter 8

---
+++ Part_I:_Basics/Chapter_8:_Functions/8-1.Message.py
@@ -0,0 +1,6 @@
+def display_message():
+    print("Hello everyone, in this chapter I'll learn about functions in Python, "
+          "which are named blocks of code designed to perform a specific task. "
+          "Have a nice day!")
+
+display_message()
