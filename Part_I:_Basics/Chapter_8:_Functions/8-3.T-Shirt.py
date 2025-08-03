075428d5a35a7e4e91f633a9914a8eba67133fc1
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-03 17:28:24 -0600

    Chapter 8

---
+++ Part_I:_Basics/Chapter_8:_Functions/8-3.T-Shirt.py
@@ -0,0 +1,8 @@
+def make_shirt(size, text):
+    print(f"\nThe size of the t-shirt is {size} and the text printed on it is '{text}'.")
+
+# Using positional arguments.
+make_shirt('L', 'I use Arch btw')   
+
+# Using keyword arguments.
+make_shirt(size='L', text='I use Arch btw')
