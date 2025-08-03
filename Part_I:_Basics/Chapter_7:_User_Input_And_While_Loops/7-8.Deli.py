435968edde6ce4d1c17a8f10c11f63587931ade4
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-03 14:34:07 -0600

    Chapter 7

---
+++ Part_I:_Basics/Chapter_7:_User_Input_And_While_Loops/7-8.Deli.py
@@ -0,0 +1,16 @@
+sandwich_orders = ['Club', 'Cuban', 'Egg salad', 'Chicken',
+                   'Monte Cristo', 'Ham and cheese',]
+finished_sandwiches = []
+
+
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()        # get the last item of the list
+
+    print(f"\nI made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)    # move it to the list of finished sandwiches
+
+# print each sandwich that was made
+print("\n---Each sandwich that was made---\n") 
+for finished_sandwich in finished_sandwiches:
+    print(f"- {finished_sandwich} sandwich.")
