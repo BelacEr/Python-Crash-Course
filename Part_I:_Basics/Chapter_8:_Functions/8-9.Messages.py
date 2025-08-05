28b7f121754f6d6ce142666a96dc7fe416b702b4
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-04 19:26:51 -0600

    Chapter 8

---
+++ Part_I:_Basics/Chapter_8:_Functions/8-9.Messages.py
@@ -0,0 +1,12 @@
+def show_messages(messages):
+    for message in messages:
+        print('\n', message)
+
+short_texts = [
+    "Todo lo que has vivido y sigues viviendo es una mentira, engaño, oculto de ti la vida y la muerte.",
+    "Destino del hombre es vivir en las convulsiones de la angustia o en el letargo del tedio",
+    "El trabajo nos libra de tres insufribles calamidades: el aburrimiento, el vicio y la necesidad.",
+    "Trabajemos, pues, sin argumentar -dijo Martín- que es el único medio de que sea la vida tolerable.",
+        ]
+if __name__ == '__main__':
+    show_messages(short_texts)
