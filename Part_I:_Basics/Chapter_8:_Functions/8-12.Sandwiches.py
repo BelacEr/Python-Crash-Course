16f287247b78d885755808e170e02335d00720f8
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-07 13:14:52 -0600

    Chapter 8

--- Part_I:_Basics/Chapter_8:_Functions/8-12.Sandwiches.py
+++ Part_I:_Basics/Chapter_8:_Functions/8-12.Sandwiches.py
@@ -1,8 +1,11 @@
-def sandwich(*toppings):
-    pass
+def sandwich(*ingredients):  
+    print("\nThe customer wants a sandwich with the following ingredients:")
+    for ingredient in ingredients:  
+        print(f"- {ingredient}")
 
-
-
-
+# Call the function three times, using a difffent number of arguments each time.
 if __name__ == '__main__':
-    sandwich()
+    sandwich("bread", "lettuce", "tomato", "mayo")
+    sandwich('bread', 'cheese', 'mayonnaise', 'ham', 'tomatoes')
+    sandwich('bread', 'avocado', 'mayonnaise', 'tuna salad')
+
