58efe4fefb96ad2ce428dd50bdabfbd7950a6551
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-07 14:42:34 -0600

    Chapter 8

---
+++ Part_I:_Basics/Chapter_8:_Functions/8-16.Imports.py
@@ -0,0 +1,19 @@
+import imports_functions
+
+imports_functions.describe_city('Mexico City')
+
+from imports_functions import describe_city 
+
+describe_city('Guadalajara')
+
+from imports_functions import describe_city as dc
+
+dc('Monterrey')
+
+import imports_functions as imfu
+
+imfu.describe_city('Queretaro')
+
+from imports_functions import *
+
+describe_city('Veracruz')
