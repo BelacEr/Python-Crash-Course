a49a5c65a6a31df6e660f74dd79ae1c0dda5d761
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-10 16:31:28 -0600

    Chapter 9

---
+++ Part_I:_Basics/Chapter_9:_Classes/9-10.Imported_Restaurant.py
@@ -0,0 +1,6 @@
+import restaurant_module 
+
+restaurant = restaurant_module.Restaurant("Heart & Stone", "Italian")    # restaurant instance
+
+restaurant.open_restaurant()
+restaurant.describe_restaurant()
