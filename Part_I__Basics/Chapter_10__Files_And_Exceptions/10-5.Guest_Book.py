444f5b4be2b240d1f12418a6a753d3c68f06a46a
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-14 19:33:44 -0600

    Chapter 10

--- Part_I__Basics/Chapter_10__Files_And_Exceptions/10-5.Guest_Book.py
+++ Part_I__Basics/Chapter_10__Files_And_Exceptions/10-5.Guest_Book.py
@@ -3,14 +3,14 @@ from pathlib import Path
 path = Path("guest_book.txt")
 
 a = 0           
-users = ""      # Create an emtpy string to hold the users
+users = ""                                      # Creates an empty string to store user names.
 active = True
-while active:                                # A loop that repeats until someone types 'q'
+while active:                                   # A loop that repeats until someone types 'q'.
     print("\nType 'q' to finish")
     user_input = input("What is your name? ")   # Ask for the user's name.
     if user_input == 'q':
-        active = False
+        active = False                          # Break the loop.
     else:
         users += user_input
-        users += "\n"                           # Each entry appears on a new line
+        users += "\n"                           # Each entry appears on a new line.
 path.write_text(users)
